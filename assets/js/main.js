e8811ab872221d581a6866aa4751f30cb8eefccb
Cleber Augusto <57002120+CleberAfr@users.noreply.github.com>
2021-11-10 22:03:44 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,9 @@ const navMenu = document.getElementById('nav-menu'),
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+    navToggle.addEventListener('click', function () {
+            navMenu.classList.add('show-menu')
+        })
 }
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
